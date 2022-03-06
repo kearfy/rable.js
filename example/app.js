80233081711f101482bb9d2db0d0f7ec8afbdba6
Micha de Vries <contact@michadevries.nl>
2022-03-06 11:39:32 +0100

    Updated examples.

--- example/app.js
+++ example/app.js
@@ -1,20 +1,23 @@
 import { Rable } from '../rable.mjs';
 
+// BUTTON COUNTER EXAMPLE.
+
 const buttonCounter = new Rable({
     data: {
         counter: 0
     }
-});
+}).mount('#button-counter');
 
-buttonCounter.mount('#button-counter');
+// NOTE TAKING APP.
 
 const note = new Rable({
     data: {
+        newnote: '',
         notes: [
+            "Hello world!",
             "Get some groceries.",
             "Close the issues."
         ],
-        newnote: '',
         addNote() {
             if (this.newnote !== '') {
                 this.notes.push(this.newnote);
@@ -25,6 +28,43 @@ const note = new Rable({
             this.notes.splice(index, 1);
         }
     }
-});
+}).mount('#notes');
 
-note.mount('#notes');
+// CLICKS PER SECOND TEST.
+
+const cps = new Rable({
+    data: {
+        counter: 0,
+        running: false,
+        listening: true,
+        buttonContent: "CPS test",
+        buttonClicked: false,
+        clickHandler() {
+            this.buttonClicked = true;
+            const waitUserFinished = () => setTimeout(() => {
+                if (this.buttonClicked) {
+                    this.buttonClicked = false;
+                    waitUserFinished();
+                } else {
+                    this.listening = true;
+                }
+            }, 1000);
+
+            if (this.listening) {
+                this.running = true;
+                this.listening = false;
+                setTimeout(() => {
+                    this.running = false;
+                    this.buttonContent = `${this.counter / 5} CPS!`;
+                    this.counter = 0;
+                    waitUserFinished();
+                }, 5000);
+            }
+
+            if (this.running) {
+                this.counter++;
+                this.buttonContent = this.counter;
+            }
+        }
+    }
+}).mount('#cps');
